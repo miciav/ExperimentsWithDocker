b720a4729b3f446468abdbfbcb51f098b1462c17
miciav <michele.ciavotta@gmail.com>
2016-09-05 18:39:52 +0200

    kafka ready for swarm

--- peeping_data_stream.sh
+++ peeping_data_stream.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-kafkacat -C -b localhost:9092 -t my_topic
+
+# kafkacat -C -b localhost:9092 -t my_topic
+docker exec -it kafka /opt/kafka_2.11-0.10.0.1/bin/./kafka-console-consumer.sh --zookeeper zookeeper --topic my_topic
 
