befa4e9aee30cab5f697d7da04ed8a440fac7e31
miciav <michele.ciavotta@gmail.com>
2016-09-08 13:39:39 +0200

    more data

--- run_plaftform.sh
+++ run_plaftform.sh
@@ -2,4 +2,14 @@
 #export HOSTIP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 #sed -e "s/HOSTIP/${HOSTIP}/g" docker-compose.yml | docker-compose --file - up -d
 
+sudo su <<HERE
+rm -rf cassandra
+rm -rf data
+rm -rf spark
+rm -rf zeppelin
+HERE
+tar -xzvf cassandra.tgz
+tar -xzvf data.tgz
+tar -xzvf spark.tgz
+tar -xzvf zeppelin.tgz
 docker-compose up -d 
